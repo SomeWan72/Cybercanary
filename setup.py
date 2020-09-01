f541fb6b762ec79a4cd9dee120fef8e56cd39a2a
SomeWan72 <jugr98@gmail.com>
2020-09-01 20:07:50 +0200

    Requirement and setup files added to the project.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='Canary',
+    version='0.5',
+    packages=[
+        'IPy',
+        'webthing',
+        'psutil'
+    ],
+    license='',
+    author='somewan',
+    author_email='somewanprojects@gmail.com',
+    description='Low cost honeypot',
+    url='https://github.com/SomeWan72/Canary',
+    download_url='https://github.com/SomeWan72/Canary/archive/master.tar.gz'
+)
