c8348c09ee8297089ace729cb622a51de47b581f
SomeWan72 <jugr98@gmail.com>
2020-09-03 20:46:32 +0200

    Some changes to see if the fu**ing snapcraft decides to work.

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
+#!/usr/bin/python
+
 from distutils.core import setup
 
 setup(
     name='Canary',
     version='0.5',
+    license='',
     packages=[
         'IPy',
         'webthing',
         'psutil'
     ],
-    license='',
     author='somewan',
     author_email='somewanprojects@gmail.com',
     description='Low cost honeypot',
